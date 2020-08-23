ab0701b8b2084ff5275d6b67fdb83bd7c71ab830
Mallory Eastburn <mallorykaitlin@gmail.com>
2020-08-22 19:41:28 -0500

    Adjusted JS
    Created background change function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-//Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
-
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
-
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+const rainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
+function randomNumber() {
+  return Math.floor(Math.random() * rainbow.length);
 }
+
+function changeColor() {
+  document.querySelector("body").style.backgroundColor =
+    rainbow[randomNumber()];
+}
+const changeColorButton = document.querySelector("button");
+changeColorButton.addEventListener("click", changeColor);
